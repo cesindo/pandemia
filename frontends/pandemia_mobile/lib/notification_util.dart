ef8513116884393ff185ca152b430a6cab5f23bb
fatkhur1960 <fatkhuranonym@gmail.com>
2020-03-24 23:17:15 +0700

    Menambahkan data source untuk mendapatkan kasus covid-19 per-provinsi

--- frontends/pandemia_mobile/lib/notification_util.dart
+++ frontends/pandemia_mobile/lib/notification_util.dart
@@ -117,7 +117,7 @@ class NotificationUtil {
   }
 
   void _sendFCMToken(BuildContext context) {
-    var fcmBloc = BlocProvider.of<FcmBloc>(context);
+    FcmBloc fcmBloc = BlocProvider.of<FcmBloc>(context);
     fcmBloc.dispatch(CreateFcm());
   }
 
