f02ed7eba02772a73e0cb280f82f671d5e833fd4
robin <robin@ansvia.com>
2020-03-30 16:46:57 +0700

    perbagus tampilan tab stats

--- frontends/pandemia_mobile/lib/screens/stats/stats_page.dart
+++ frontends/pandemia_mobile/lib/screens/stats/stats_page.dart
@@ -26,12 +26,12 @@ class StatsPage extends StatelessWidget {
           child: LoadingIndicator(),
         );
       } else if (state is StatsLoaded) {
-        print("state.items: ${state.items}");
+        // print("state.items: ${state.items}");
         state.items.forEach((a) {
           viewItems.add(Section(a.loc.toUpperCase(), a));
         });
       } else if (state is StatsUpdated) {
-        print("state.items: ${state.items}");
+        // print("state.items: ${state.items}");
         state.items.forEach((a) {
           viewItems.add(Section(a.loc.toUpperCase(), a));
         });
@@ -53,47 +53,53 @@ class Section extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final numfa = new NumberFormat("#,##0", "en_US");
-    return Container(
+    return Card(
+      elevation: 2.0,
+      margin: new EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(bottom: 10),
-            child: Row(children:<Widget>[Expanded(child:Text(
-              this.title,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-              textAlign: TextAlign.left,
-            )),Text(
-                    timeago.format(TimeHelper.parseAsUtc(this.data.lastUpdated)),
-                    style: TextStyle(fontSize: 15, color: Colors.grey),
-                  )]),
+            padding: EdgeInsets.all(10),
+            child: Row(children: <Widget>[
+              Expanded(
+                  child: Text(
+                this.title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                textAlign: TextAlign.left,
+              )),
+              Text(
+                timeago.format(TimeHelper.parseAsUtc(this.data.lastUpdated)),
+                style: TextStyle(fontSize: 15, color: Colors.grey),
+              )
+            ]),
           ),
-          Table(
-            columnWidths: {0: FlexColumnWidth(0.5), 1: FixedColumnWidth(20)},
-            children: [
-              TableRow(children: [
-                Text("Total kasus"),
-                Text(":"),
-                Text(numfa.format(data.totalCases))
-              ]),
-              TableRow(children: [
-                Text("Total kematian"),
-                Text(":"),
-                Text(numfa.format(data.totalDeaths))
-              ]),
-              TableRow(children: [
-                Text("Total sembuh"),
-                Text(":"),
-                Text(numfa.format(data.totalRecovered))
-              ])
-            ],
-          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Table(
+              columnWidths: {0: FlexColumnWidth(0.5), 1: FixedColumnWidth(20)},
+              children: [
+                TableRow(children: [
+                  Text("Total kasus"),
+                  Text(":"),
+                  Text(numfa.format(data.totalCases))
+                ]),
+                TableRow(children: [
+                  Text("Total kematian"),
+                  Text(":"),
+                  Text(numfa.format(data.totalDeaths))
+                ]),
+                TableRow(children: [
+                  Text("Total sembuh"),
+                  Text(":"),
+                  Text(numfa.format(data.totalRecovered))
+                ])
+              ],
+            ),
+          )
         ],
       ),
-      padding: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: Colors.grey, width: 1))),
     );
   }
 }
